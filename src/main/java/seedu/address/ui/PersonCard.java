94be209fd8c99ad3416b4a4ca1c0dce6ed4f56ea
gabyang <gabrielyang@berkeley.edu>
2022-10-26 14:06:10 +0800

    More UI fixes

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -65,20 +65,21 @@ public class PersonCard extends UiPart<Region> {
         phone.setText("\uD83D\uDCDE\t" + person.getPhone().value);
         address.setText("\uD83C\uDFE0\t" + person.getAddress().value);
         email.setText("\uD83D\uDCE7\t" + person.getEmail().value);
-
-        // replace birthday and insurance once implemented.
-        // insurance.setText("\uD83D\uDCC4\t" + "something something");
-
-        healthInsurance.setText("Health               "
-                + (person.getHealthInsurance().getHasInsurance() ? "\u2705" : "\u274e"));
-        disabilityInsurance.setText("Disability           "
-                + (person.getDisabilityInsurance().getHasInsurance() ? "\u2705" : "\u274e"));
-        criticalIllnessInsurance.setText("Critical Illness    "
-                + (person.getCriticalIllnessInsurance().getHasInsurance() ? "\u2705" : "\u274e"));
-        lifeInsurance.setText("Life                    "
-                + (person.getLifeInsurance().getHasInsurance() ? "\u2705" : "\u274e"));
         birthday.setText("\uD83C\uDF82\t"
                 + person.getBirthday().value.format(DateTimeFormatter.ofPattern(DATE_FORMAT_PATTERN)));
+        healthInsurance.setText(
+                (person.getHealthInsurance().getHasInsurance() ? "\u2705\t" : "\u274c\t") +
+                        "Health Policy");
+        disabilityInsurance.setText(
+                (person.getDisabilityInsurance().getHasInsurance() ? "\u2705\t" : "\u274c\t") +
+                        "Disability Policy");
+        criticalIllnessInsurance.setText(
+                (person.getCriticalIllnessInsurance().getHasInsurance() ? "\u2705\t" : "\u274c\t") +
+                        "Critical Illness Policy");
+        lifeInsurance.setText(
+                (person.getLifeInsurance().getHasInsurance() ? "\u2705\t" : "\u274c\t") +
+                "Life Policy");
+
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
